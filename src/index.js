f506e2b60dedcbc6bbf83053a8907c7687294a48
yarraf <arraf.youness@gmail.com>
2020-04-14 15:14:39 +0100

    create store redux,home,actions,reducers

--- src/index.js
+++ src/index.js
@@ -7,8 +7,16 @@ import OverView from './Components/OverView/OverView';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import store from './store';
+import {Provider} from 'react-redux';
+import HomeContainer from './Components/home/HomeContainer';
 
-ReactDOM.render(<OverView Name ="hello" />,document.getElementById('root'));
+
+ReactDOM.render(
+<Provider store={store}>
+    <HomeContainer />
+</Provider>
+,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
